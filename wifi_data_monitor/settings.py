8985938c4268360efaf31265f01ac5602f48a30c
vincentiano23 <vincentmuthiani15@gmail.com>
2025-04-20 17:47:36 +0300

    fix: static directory warning resolved

--- wifi_data_monitor/settings.py
+++ wifi_data_monitor/settings.py
@@ -61,9 +61,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/dashboard/'
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static", 
-]
-
-STATIC_ROOT = BASE_DIR / "staticfiles"  
+STATIC_ROOT = BASE_DIR / 'staticfiles'
